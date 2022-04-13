33eca2c84996df6eff134a8cafd6c500cda0095c
haimaco <102148627+haimaco@users.noreply.github.com>
2022-04-13 15:59:20 +0300

    Create exercise 4.5.js

---
+++ exercise 4.5.js
@@ -0,0 +1,11 @@
+const leapyear = (year) => {
+     if (year % 4 === 0) {
+      console.log("It is indeed a leap year");
+    } else if (year % 400 === 0) {
+      console.log("It is indeed a leap year");
+    } else if  (year % 100 === 0) {
+      console.log("This is not a leap year.");
+    } else {
+      console.log("This is not a leap year.");
+    }
+  };
