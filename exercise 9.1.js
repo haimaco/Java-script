ea57d9d9b11db0f877290e458a85ce8fcbc6b639
haimaco <102148627+haimaco@users.noreply.github.com>
2022-04-13 16:05:52 +0300

    Create exercise 9.1.js

---
+++ exercise 9.1.js
@@ -0,0 +1,11 @@
+const ListNeighbours = [
+    ["Canada", "Mexico"],
+    ["Spain"],
+    ["Norway", "Sweden", "Russia"],
+  ];
+  
+  for (let i = 0; i < Neighbours.length; i++) {
+    for (let j = 0; j < Neighbours[i].length; j++) {
+      console.log(`Neighbour: ${Neighbours[i][j]}`);
+    }
+  };
