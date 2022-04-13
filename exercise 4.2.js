9d7b31df7d50a0ce39152394bb2e44b7bfe09fc6
haimaco <102148627+haimaco@users.noreply.github.com>
2022-04-13 15:56:06 +0300

    Create exercise 4.2.js

---
+++ exercise 4.2.js
@@ -0,0 +1,15 @@
+const Score = (score) => {
+    if (score >= 0 && score <= 64) {
+      return "F";
+    } else if (score >= 65 && score <= 69) {
+      return "D";
+    } else if (score >= 70 && score <= 79) {
+      return "C";
+    } else if (score >= 80 && score <= 89) {
+      return "B";
+    } else if (score >= 90 && score <= 100) {
+      return "A";
+    } else {
+      return " The score  have to be a number between 0 and 100";
+    }
+  };
