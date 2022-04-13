805cf2b3493cd3c1387aec0933efd94ba6263044
haimaco <102148627+haimaco@users.noreply.github.com>
2022-04-13 17:37:07 +0300

    Create exercise 12.1.js

---
+++ exercise 12.1.js
@@ -0,0 +1,29 @@
+var library = [
+    {
+      author: "Bill Gates",
+      title: "The Road Ahead",
+      readingStatus: true,
+    },
+    {
+      author: "Steve Jobs",
+      title: "Walter Isaacson",
+      readingStatus: true,
+    },
+    {
+      author: "Suzanne Collins",
+      title: "Mockingjay: The Final Book of The Hunger Games",
+      readingStatus: false,
+    },
+  ];
+  
+  const read = (data) => {
+    data.forEach((ele) => {
+      if (ele.readingStatus === true) {
+        console.log(
+          `\nBook Name: ${ele.title}\nAuthor Name: ${ele.author}\nReading Status: ${ele.readingStatus}\n`
+        );
+      }
+    });
+  };
+  
+  read(library);
