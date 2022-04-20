a4bfb76caf7df6b867d1ec0363dc02a2bc563d03
Haim Acoca <102148627+haimaco@users.noreply.github.com>
2022-04-20 19:26:47 +0300

    Create exercise 15.3.js

---
+++ exercise 15.3.js
@@ -0,0 +1,5 @@
+function getSumOfEven(arr) {
+  // return arr[2] + arr[4] + arr[6] + arr[8] + arr[10];
+  return arr[1] + arr[3] + arr[5] + arr[7] + arr[9];
+}
+getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
