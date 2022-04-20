e8644ecc9e49d6aade7792e7aad24f3edab541b5
Haim Acoca <102148627+haimaco@users.noreply.github.com>
2022-04-20 19:26:19 +0300

    Create exercise 15.2.js

---
+++ exercise 15.2.js
@@ -0,0 +1,11 @@
+function  findSmalest(a, b, c) {
+    if (a > b > c) {
+      return c;
+    } else if (a > c > b) {
+      return a;
+    } else {
+      return b;
+    }
+  }
+  
+  findSmalest(52, 66, 2);
